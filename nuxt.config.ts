4265bbfdcd29badce9a281ced7bcc7b6f1f543d5
Marshalll99 <mirmansoor100@gmail.com>
2025-01-24 21:20:27 +0530

    Remove unused components and styles, update nuxt.config.ts for compatibility, and adjust Tailwind CSS configuration. This commit deletes several Vue components, including app.vue, main.css, and various feature-related components, streamlining the project structure. The nuxt.config.ts file is updated to set a new compatibility date and disable the appManifest feature, while the Tailwind CSS configuration is modified to include the app directory for content scanning.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  experimental: { appManifest: false },
+  // compatibilityDate: "2024-11-01",
+  future: {
+    compatibilityVersion: 4,
+  },
+
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
+  compatibilityDate: "2025-01-24",
 });
